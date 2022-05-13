6ba5099596ade5b203e1930b90ea6f45a84bc2c3
Sunghyun627 <kksscc4@gmail.com>
2022-05-13 12:01:16 +0900

    Send HTTP post request to firebase

--- src/Ingredients/Ingredients.js
+++ src/Ingredients/Ingredients.js
@@ -1,15 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import IngredientList from './IngredientList';
 import IngredientForm from './IngredientForm';
 import Search from './Search';
 
 function Ingredients() {
   const [userIngredients, setUserIngredients] = useState([]);
+  useEffect(() => {
+    fetch(
+      'https://react-hooks-update-60525-default-rtdb.firebaseio.com/ingredients.json'
+    )
+      .then((response) => response.json())
+      .then((responseData) => {
+        const loadedIngredients = [];
+        for (const key in responseData) {
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount,
+          });
+        }
+        setUserIngredients(loadedIngredients);
+      });
+  });
   const addIngredientHandler = (ingredient) => {
-    setUserIngredients((prevIngredients) => [
-      ...prevIngredients,
-      { id: Math.random().toString(), ...ingredient },
-    ]);
+    fetch(
+      'https://react-hooks-update-60525-default-rtdb.firebaseio.com/ingredients.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(ingredient),
+        headers: { 'Content-Type': 'application/json' },
+      }
+    )
+      .then((response) => response.json())
+      .then((responseData) =>
+        setUserIngredients((prevIngredients) => [
+          ...prevIngredients,
+          { id: responseData.name, ...ingredient },
+        ])
+      );
   };
   const removeIngredientHandler = (ingredientId) => {
     setUserIngredients((prevIngredients) =>
