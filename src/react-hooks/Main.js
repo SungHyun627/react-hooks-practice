71682ba8cafc8477f1c0ae8276c2cfe2be8fae56
Sunghyun627 <kksscc4@gmail.com>
2022-08-15 09:50:08 +0900

    feat: create useTabs hook

--- src/react-hooks/Main.js
+++ src/react-hooks/Main.js
@@ -1,12 +1,42 @@
-import { useInput } from './useInput';
+import useInput from './useInput';
+import useTabs from './useTabs';
+import useTitle from './useTitle';
+
+const contents = [
+  {
+    tab: 'Section 1',
+    contentDetail: "I'm the content of the Section 1",
+  },
+  {
+    tab: 'Section 2',
+    contentDetail: "I'm the content of the Section 2",
+  },
+  {
+    tab: 'Section 3',
+    contentDetail: "I'm the content of the Section 3",
+  },
+];
 
 const Main = () => {
   const maxLen = (value) => !value.includes('@');
   const name = useInput('Kim', maxLen);
+
+  const { currentItem, changeItemIndexHandler } = useTabs(0, contents);
+  const titleUpdater = useTitle('Loading');
+  setTimeout(() => {
+    titleUpdater('home');
+  }, 5000);
+
   return (
     <div className="Main">
       <h1>Use custom react hooks</h1>
       <input placeholder="name" {...name} />
+      {contents.map((content, idx) => (
+        <button onClick={() => changeItemIndexHandler(idx)}>
+          {content.contentDetail}
+        </button>
+      ))}
+      <div>{currentItem.contentDetail}</div>
     </div>
   );
 };
---
+++ src/react-hooks/useTabs.js
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+
+const useTabs = (initialTab, allTabs) => {
+  const [currentIndex, setCurrentIndex] = useState(initialTab);
+  if (!allTabs || !Array.isArray(allTabs)) {
+    return;
+  }
+  return {
+    currentItem: allTabs[currentIndex],
+    changeItemIndexHandler: setCurrentIndex,
+  };
+};
+
+export default useTabs;
